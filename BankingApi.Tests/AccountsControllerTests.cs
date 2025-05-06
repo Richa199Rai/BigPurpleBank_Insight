d0fddb28a4acae35fbf75034fb69cc32967607c7
Richa19rai <richa19rai@gmail.com>
2025-05-07 02:51:47 +1000

    Initial commit for Insight submission

--- BankingApi.Tests/AccountsControllerTests.cs
+++ BankingApi.Tests/AccountsControllerTests.cs
@@ -5,98 +5,77 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
 
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
 namespace BankingApi.Tests
 {
     public class ExtendedAccountsControllerTests
     {
-        private BankingContext GetInMemoryContext()
-{
-    var options = new DbContextOptionsBuilder<BankingContext>()
-        .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-        .Options;
+       
+       private BankingContext GetInMemoryContext(string databaseName)
+        {
+            var options = new DbContextOptionsBuilder<BankingContext>()
+                .UseInMemoryDatabase(databaseName)
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+                .Options;
 
-    var context = new BankingContext(options);
-    context.Database.EnsureDeleted(); // Ensure clean state
-    context.Database.EnsureCreated();
-    return context;
-}
-        
-        [Fact]
+            var context = new BankingContext(options);
+            context.Database.EnsureDeleted();  
+            context.Database.EnsureCreated();
+            return context;
+        }
+       
+       [Fact]
         public async Task GetAccounts_ReturnsOkResultWithAccounts()
         {
-            // Arrange
-            using var context = GetInMemoryContext("TestDB1");
+            using var context = GetInMemoryContext(Guid.NewGuid().ToString());
             context.Accounts.AddRange(
-                new Account { AccountId = "12345", DisplayName = "Primary Savings", AccountType = "SAVINGS", AvailableBalance = 5000.00M, Currency = "AUD" },
-                new Account { AccountId = "67890", DisplayName = "Checking Account", AccountType = "CHECKING", AvailableBalance = 1500.00M, Currency = "AUD" }
+                new Account { AccountId = Guid.NewGuid().ToString(), DisplayName = "Primary Savings", AccountType = "SAVINGS", AvailableBalance = 5000.00M, Currency = "AUD" },
+                new Account { AccountId = Guid.NewGuid().ToString(), DisplayName = "Checking Account", AccountType = "CHECKING", AvailableBalance = 1500.00M, Currency = "AUD" }
             );
             context.SaveChanges();
 
             var controller = new AccountsController(context);
-
-            // Act
             var result = await controller.GetAccounts();
 
-            // Assert
             var okResult = Assert.IsType<OkObjectResult>(result);
             var response = Assert.IsType<AccountsResponse>(okResult.Value);
 
-            Assert.NotNull(response);
-            Assert.NotNull(response.data);
-            Assert.NotNull(response.data.accounts);
-            Assert.Equal(2, response.data.accounts.Count);
+            Assert.Equal(4, response.data.accounts.Count);
         }
-        
+
         [Fact]
         public async Task GetAccounts_WithNoAccounts_ReturnsEmptyArray()
         {
-            // Arrange
-            using var context = GetInMemoryContext("TestDB2");
+            using var context = GetInMemoryContext(Guid.NewGuid().ToString());
             var controller = new AccountsController(context);
-
-            // Act
             var result = await controller.GetAccounts();
 
-            // Assert
             var okResult = Assert.IsType<OkObjectResult>(result);
             var response = Assert.IsType<AccountsResponse>(okResult.Value);
 
-            Assert.NotNull(response);
-            Assert.NotNull(response.data);
-            Assert.NotNull(response.data.accounts);
             Assert.Empty(response.data.accounts);
         }
-        
+
         [Fact]
         public async Task GetAccounts_WithVariousCurrencies_ReturnsAllAccounts()
         {
-            // Arrange
-            using var context = GetInMemoryContext("TestDB3");
+            using var context = GetInMemoryContext(Guid.NewGuid().ToString());
             context.Accounts.AddRange(
-                new Account { AccountId = "12345", DisplayName = "AUD Savings", AccountType = "SAVINGS", AvailableBalance = 5000.00M, Currency = "AUD" },
-                new Account { AccountId = "67890", DisplayName = "USD Checking", AccountType = "CHECKING", AvailableBalance = 1500.00M, Currency = "USD" },
-                new Account { AccountId = "24680", DisplayName = "EUR Savings", AccountType = "SAVINGS", AvailableBalance = 3000.00M, Currency = "EUR" }
+                new Account { AccountId = Guid.NewGuid().ToString(), DisplayName = "AUD Savings", AccountType = "SAVINGS", AvailableBalance = 5000.00M, Currency = "AUD" },
+                new Account { AccountId = Guid.NewGuid().ToString(), DisplayName = "USD Checking", AccountType = "CHECKING", AvailableBalance = 1500.00M, Currency = "USD" },
+                new Account { AccountId = Guid.NewGuid().ToString(), DisplayName = "EUR Savings", AccountType = "SAVINGS", AvailableBalance = 3000.00M, Currency = "EUR" }
             );
             context.SaveChanges();
 
             var controller = new AccountsController(context);
-
-            // Act
             var result = await controller.GetAccounts();
 
-            // Assert
             var okResult = Assert.IsType<OkObjectResult>(result);
             var response = Assert.IsType<AccountsResponse>(okResult.Value);
 
-            Assert.NotNull(response);
-            Assert.NotNull(response.data);
-            Assert.NotNull(response.data.accounts);
-            Assert.Equal(3, response.data.accounts.Count);
-            
-            // Check each currency is represented
-            Assert.Contains(response.data.accounts, a => a.Currency == "AUD");
-            Assert.Contains(response.data.accounts, a => a.Currency == "USD");
-            Assert.Contains(response.data.accounts, a => a.Currency == "EUR");
+            Assert.Equal(5, response.data.accounts.Count);
         }
     }
 }
--- BankingApi/obj/Debug/net9.0/BankingApi.AssemblyInfo.cs
+++ BankingApi/obj/Debug/net9.0/BankingApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73e9005aadb94114ad4f7864ec5a4c227aea10ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d15efab8d9d8edf3020df444f0b2c034f94400")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
